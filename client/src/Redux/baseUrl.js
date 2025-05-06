1a98a51dcecc2ba3f8aa5e9f8d97a0e304aff75d
Parmod Kumar <yparmodyadav@gmail.com>
2025-05-06 11:42:32 +0530

    frontend base url issue fixed

--- client/src/Redux/baseUrl.js
+++ client/src/Redux/baseUrl.js
@@ -1,13 +1,10 @@
 // Base url
 let BASE_URL;
-if(import.meta.VITE_ === "production") {
-  BASE_URL = import.meta.env.VITE_PROD_URL;
-} else if(import.meta.env.VITE_ === "development") {
-  BASE_URL = import.meta.env.VITE_DEV_URL;
-} else if(import.meta.env.VITE_ === "test") {
-  BASE_URL = import.meta.env.VITE_TEST_URL;
+
+if (import.meta.env.VITE_ENVIRONMENT === "production") {
+  BASE_URL = import.meta.env.VITE_BASE_URL_PROD;
 } else {
-  BASE_URL = import.meta.env.VITE_LOCAL_URL;
+  BASE_URL = import.meta.env.VITE_BASE_URL_DEV;
 }
 
 export default BASE_URL;
